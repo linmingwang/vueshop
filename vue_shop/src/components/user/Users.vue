6d708023a6cda638577c4bf46e749d5d841208c0
v_linmingwang <964280219@qq.comgit commit -m first>
2020-09-03 18:17:59 +0800

    提交用户页面

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -63,7 +63,20 @@
       :visible.sync="addDialogVisible"
       width="50%">
       <!-- 内容主体区域 -->
-      <span>这是一段信息</span>
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email" type="email" />
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile" />
+        </el-form-item>
+      </el-form>
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
@@ -88,7 +101,28 @@ export default {
       userList: [],
       // 用户总数
       total: 0,
-      addDialogVisible: false
+      // 是否是否添加用户对话框
+      addDialogVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加用户的表单验证规则对象
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '密码的长度在6~15个字符之间', trigger: 'blur' }
+        ],
+        email: [{ required: true, message: '请输入邮箱', trigger: 'blur' }],
+        mobile: [{ required: true, message: '请输入手机', trigger: 'blur' }]
+      }
     }
   },
   created() {
